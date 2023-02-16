c78c368b7fc92725a544b1559dd231cb81e3d0ca
NightUmbrella <>
2023-02-17 01:36:02 +0500

    First commit start 1:35AM

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import {Hero, Navbar, Footer} from "./Components"
+import { hero } from "./data/travigoData";
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <Hero/>
+      <Hero hero={hero}/>
       <Footer />
     </>
   );
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 
-const Hero = () => {
+const Hero = ({hero: {title,subtitle, btn1, btn2, text,img}}) => {
   return (
     <>
       <div className="">
         <div className="">
           <div className="">
-            <h1 className=""></h1>
-            <h1 className=""></h1>
-            <p className=""></p>
+            <h1 className="">{title}</h1>
+            <h1 className="">{subtitle}</h1>
+            <p className="">{text}</p>
             <div className="">
-              <button type="button" className=""></button>
-              <button type="button" className=""></button>
+              <button type="button" className="">{btn1}</button>
+              <button type="button" className="">{btn2}</button>
             </div>
           </div>
           <div className="">
